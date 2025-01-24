b41e4f95ab836848f3f8c57360e4719983a6d0e8
Jakob Sanowski <jakob.sanowski@sanbach.de>
2025-01-24 16:12:05 +0100

    ravedude: Fix `flake.nix` using outdated `rustc`

--- ravedude/flake.nix
+++ ravedude/flake.nix
@@ -1,36 +1,43 @@
 {
   inputs = {
-    utils = {
-      url = "github:numtide/flake-utils";
-    };
+    utils = { url = "github:numtide/flake-utils"; };
 
-    naersk = {
-      url = "github:nix-community/naersk";
+    naersk = { url = "github:nix-community/naersk"; };
+
+    nixpkgs-mozilla = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, nixpkgs-mozilla, utils, naersk }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
+
+          overlays = [ (import nixpkgs-mozilla) ];
         };
 
-        naersk' = pkgs.callPackage naersk {};
+        toolchain = (pkgs.rustChannelOf {
+          rustToolchain = ./rust-toolchain.toml;
+          sha256 = "sha256-lMLAupxng4Fd9F1oDw8gx+qA0RuF7ou7xhNU8wgs0PU=";
+        }).rust;
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
 
         lib = pkgs.lib;
 
-      in
-      rec {
+      in rec {
         packages.default = naersk'.buildPackage {
           pname = "ravedude";
           src = ./.;
 
-          buildInputs = with pkgs; lib.optionals pkgs.stdenv.isLinux [
-            pkg-config
-            udev
-          ];
+          buildInputs = with pkgs;
+            lib.optionals pkgs.stdenv.isLinux [ pkg-config udev ];
         };
-      }
-    );
+      });
 }
